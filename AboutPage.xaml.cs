d3b5f01543dae8b9928e399b5d1efc6a95394d48
Ionut Groza <ionut.iacob.groza@gmail.com>
2023-12-09 09:40:21 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Groza_Ionut_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Groza_Ionut_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
